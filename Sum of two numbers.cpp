3e37a8af60c17aed123f42c31c7701f0576d949c
debopam525 <debopam525@GMAIL.com>
2024-12-30 19:05:02 +0530

    Finding the sum of two numbers

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout << "Enter two numbers:";
+    cin>>a;
+    cin>>b;
+    sum=a+b;
+    cout<<"Sum = "<<sum<<"\n";
+    return 0;
+}
